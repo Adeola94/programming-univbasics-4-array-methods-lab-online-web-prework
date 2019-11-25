5fb83d20771722bee75d440d858d22fae0755b35
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-11-25 19:41:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,11 +6,13 @@ end
 
 def using_sort(array)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.sort 
+array.sort = ["I"]
+array.sort = ["wow"]
 end
 
 def using_reverse(array)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.reverse 
 end
 
 def using_first(array)
