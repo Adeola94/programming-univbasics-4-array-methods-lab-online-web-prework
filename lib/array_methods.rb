123b6f6282631edc9e9b8f05dacdfbd760d0c672
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-11-25 19:42:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,17 +12,21 @@ end
 
 def using_reverse(array)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.reverse 
+array.reverse ["array"]
+array.reverse ["wow"]
 end
 
 def using_first(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.first
 end
 
 def using_last(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.last
 end
 
 def using_size(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.size
 end
